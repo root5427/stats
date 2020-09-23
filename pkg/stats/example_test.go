b7e22266d755d706b477aede6c146bc77a6b6ab7
root5427 <shukrik27@gmail.com>
2020-09-24 02:17:54 +0500

    upgrade: bank => v2.0.0

--- pkg/stats/example_test.go
+++ pkg/stats/example_test.go
@@ -3,32 +3,35 @@ package stats
 import (
 	"fmt"
 
-	"github.com/root5427/bank/pkg/types"
+	"github.com/root5427/bank/v2/pkg/types"
 )
 
 func ExampleAvg() {
 	payments := []types.Payment{
 		{
-			ID:       2,
+			ID:       1,
 			Amount:   1000,
 			Category: "Mobile",
+			Status:   "OK",
 		},
 		{
-			ID:       1,
+			ID:       2,
 			Amount:   2000,
 			Category: "Mobile",
+			Status:   "OK",
 		},
 		{
 			ID:       3,
 			Amount:   3000,
 			Category: "Withdraw",
+			Status:   "FAIL",
 		},
 	}
 
 	fmt.Println(Avg(payments))
 
 	// Output:
-	// 2000
+	// 1500
 }
 
 func ExampleTotalInCategory() {
@@ -37,20 +40,24 @@ func ExampleTotalInCategory() {
 			ID:       1,
 			Amount:   1000,
 			Category: "Mobile",
+			Status:   "OK",
 		},
 		{
 			ID:       2,
 			Amount:   2000,
 			Category: "Mobile",
+			Status:   "FAIL",
 		},
 		{
 			ID:       3,
 			Amount:   3000,
 			Category: "Withdraw",
+			Status:   "OK",
 		},
 	}
 
 	fmt.Println(TotalInCategory(payments, "Mobile"))
 
-	//Output: 3000
+	// Output:
+	// 1000
 }
